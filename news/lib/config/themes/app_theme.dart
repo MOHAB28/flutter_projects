0800058fa31625c42db75c520d8789e58e965c5e
Mohab Mahmoud <ashrafmansour@Ashrafs-MacBook-Pro.local>
2025-05-24 15:58:40 +0300

    added news pro

--- news/lib/config/themes/app_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:news/config/themes/dark_theme.dart';
-import 'package:news/config/themes/light_theme.dart';
-
-class AppTheme {
-  static ThemeData get lightThemeMode => lightTheme;
-  static ThemeData get darkThemeMode => darkTheme;
-}
--- news/lib/core/api/api_constants.dart
+++
@@ -1,7 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = "https://newsapi.org/v2";
-  static const String apiKey = "YOUR_API_KEY"; // استبدل بمفتاح API الخاص بك
-
-  // Endpoints
-  static const String topHeadlines = "/top-headlines";
-}
